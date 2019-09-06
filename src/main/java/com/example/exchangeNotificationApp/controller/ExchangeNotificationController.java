9a16bfddf258c49c6b5502b824d43f686e79c66a
CBaldemir <comertbaldemir@gmail.com>
2019-09-06 12:42:59 +0300

    remove unimportant code

--- src/main/java/com/example/exchangeNotificationApp/controller/ExchangeNotificationController.java
+++ src/main/java/com/example/exchangeNotificationApp/controller/ExchangeNotificationController.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-
 @RestController
 @RequestMapping("/exchange")
 public class ExchangeNotificationController{
@@ -26,10 +23,6 @@ public class ExchangeNotificationController{
         headers.add("token", "_magic");
         headers.add("auid", "");
         HttpEntity httpEntity = new HttpEntity(headers);
-
-        ResponseEntity<Object> responseEntity = restTemplate.exchange
-                ("https://finans.apipara.com/json/v7//market?auid=1", HttpMethod.GET, httpEntity, Object.class);
-
 
         ResponseEntity<ExchangeResponse> responseEntity2 = restTemplate.exchange
                 ("https://finans.apipara.com/json/v7//market?auid=1", HttpMethod.GET, httpEntity, ExchangeResponse.class);
